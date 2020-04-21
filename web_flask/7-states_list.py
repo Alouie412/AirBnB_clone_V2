dda65febd08d98583e555263b75ebcabe7d00f56
Alouie412 <alouie412@gmail.com>
2020-04-21 20:16:03 +0000

    Added commentary to all functions

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" This script starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,12 +8,14 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/states_list', strict_slashes=False)
 def state_list():
+    """ Render an html page with the list of states and cities """
     return render_template('7-states_list.html',
                            state=storage.all(State).values())
 
 
 @app.teardown_appcontext
 def end_session(exception):
+    """ Terminate a session to generate a new one """
     storage.close()
 
 
